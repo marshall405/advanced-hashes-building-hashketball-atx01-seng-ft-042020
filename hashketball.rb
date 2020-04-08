bdc1fcd20484900536de6ee2d93cac779c90f739
Marshall Slemp <marshall.slemp@gmail.com>
2020-04-08 16:26:56 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,1 +1,15 @@
 # Write your code here!
+def game_hash
+  {
+    :home => {
+      :team_name => "",
+      :colors => [],
+      :players => []
+    },
+    :away => {
+      :team_name => "",
+      :colors => [],
+      :players => []
+    }
+  }
+end
