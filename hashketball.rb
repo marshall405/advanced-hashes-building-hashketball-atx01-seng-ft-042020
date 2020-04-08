4deb5fc1094cd6b408292fa4378b1b538e131684
Marshall Slemp <marshall.slemp@gmail.com>
2020-04-08 16:43:02 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -5,10 +5,10 @@ def game_hash
       :team_name => "Brooklyn Nets",
       :colors => ["Black", "White"],
       :players => [
-        player("Alan Anderson", 0, 16, 22, 12, 12, 3, 1, 1),
-        player("Reggie Evans", 30, 14, 12, 12, 12, 12, 12, 7),
-        player("Brook Lopez", 11, 17, 17, 19, 10, 3, 1, 15),
-        player("Mason Plumlee", 1, 19, 26, 11, 6, 3, 8, 5),
+        player(["Alan Anderson" 0 16 22 12 12 3 1 1]),
+        player(["Reggie Evans" 30 14 12 12 12 12 12 7]),
+        player(["Brook Lopez" 11 17 17 19 10 3 1 15]),
+        player(["Mason Plumlee" 1 19 26 11 6 3 8 5]),
         player()
       ]
     },
@@ -25,13 +25,13 @@ end
 def player(stats)
   {
     :player_name => stats[0],
-    :number => stats[1],
-    :shoe => stats[2],
-    :points => stats[0],
-    :rebounds => stats[0],
-    :assists => stats[0],
-    :steals => stats[0],
-    :blocks =>stats[0],
-    :slam_dunks => slam_dunks
+    :number => stats[1].to_int,
+    :shoe => stats[2].to_int,
+    :points => stats[3].to_int,
+    :rebounds => stats[4].to_int,
+    :assists => stats[5].to_int,
+    :steals => stats[6].to_int,
+    :blocks => stats[7].to_int,
+    :slam_dunks => stats[8].to_int
   }
 end
