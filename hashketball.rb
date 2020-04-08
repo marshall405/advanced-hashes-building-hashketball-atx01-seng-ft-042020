456b984e9d3f5c4cf33f38e9b1da8fb05c60ef05
Marshall Slemp <marshall.slemp@gmail.com>
2020-04-08 16:48:12 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -5,11 +5,11 @@ def game_hash
       :team_name => "Brooklyn Nets",
       :colors => ["Black", "White"],
       :players => [
-        player(["Alan Anderson" 0 16 22 12 12 3 1 1]),
-        player(["Reggie Evans" 30 14 12 12 12 12 12 7]),
-        player(["Brook Lopez" 11 17 17 19 10 3 1 15]),
-        player(["Mason Plumlee" 1 19 26 11 6 3 8 5]),
-        player()
+        player(%w["Alan\ Anderson" 0 16 22 12 12 3 1 1]),
+        player(%w["Reggie\ Evans" 30 14 12 12 12 12 12 7]),
+        player(%w["Brook Lopez" 11 17 17 19 10 3 1 15]),
+        player(%w["Mason Plumlee" 1 19 26 11 6 3 8 5]),
+        player(%w[])
       ]
     },
     :away => {
